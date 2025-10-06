cae4a88e224152fba7fb7fa1de90736c95f1b15c
El Capitán <heneverloses@gmail.com>
2025-10-06 08:30:22 -0600

    Un código para probar el funcionamiento correcto del ESP32 y una pantalla OLED I2C.

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# display_tester_oled_i2c
+# Display Tester for ESP32
 Un código para probar el funcionamiento correcto del ESP32 y una pantalla OLED I2C.
+
+Este proyecto considera:
++ ¿La pantalla no está conectada?
+    + Quizá no la has conectado
+    + Quizá te equivocaste de pines
+    + Quizá los puertos de tu ESP32 están quemados, no sirven (Prueba con puertos alternativos)
+    + Quizá tu pantalla no funciona
++ ¿Hay problemas con la inicialización de la librería?
++ La dirección I2C del OLED se encuentra en 0x3C o 0x3D
+    + Verifica que tu código real use la dirección correcta
+
+# Componentes
++ OLED I2C (SSD1306)
++ ESP32 
+
+# Instrucciones de uso
+1. Ajusta los siguientes valores en el código `display_tester.ino`
+    + `#define SDA_PIN 32`
+        + // SDA va al GPIO 21 || también puede ser 32
+    + `#define SCL_PIN 33`
+        + // SCL va al GPIO 22 || también puede ser 33
+
+2. Opcional: Ancho y alto de la pantalla
+    + `#define SCREEN_WIDTH 128`
+    + `#define SCREEN_HEIGHT 64`
+3. Compila y carga el código en el ESP32
+
+# Teoría
+
+## ¿Qué es el protocolo I2C?
+Es un protocolo de comunicación serial síncrono de corto alcance diseñado para permitir que múltiples dispositivos maestros (como el ESP32) y múltiples dispositivos esclavos (como la pantalla OLED) se comuniquen entre sí usando solo dos cables.
+
+## ¿Cuántos puertos I2C tiene el ESP32?
+Dos controladores I2C dedicados, sin embargo el ESP32 te permite asignar estos dos puertos a casi cualquier par de pines GPIO disponibles.
+
+Convenciones comunes utilizadas por el firmware de Arduino:
+
+I2C Port 0:
++ Pines convencionales (por defecto): `GPIO 21 (SDA)` y `GPIO 22 (SCL)`	
++ Pines alternativos (por ejemplo): `GPIO 32 (SDA)` y `GPIO 33 (SCL)`
+
+I2C Port 1:
++ No tiene pines por defecto
++ Puedes usar cualquier otro par de pines libres, como GPIO 18/19 o 25/26, etcétera.
+
+## ¿Cómo funciona el programa?
+1. Verifica si la pantalla está conectada para evitar el falso positivo de display.begin()
+1. Verifica si el hardware inicia correctamente
+1. En caso de algún error, el programa lo informa lo informa a través del serial port
---
+++ display_tester_oled_i2c.ino
@@ -0,0 +1,98 @@
+// Código para probar la pantalla OLED I2C (SSD1306)
+// ESP32 - 38 pines
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// --- VARIABLES ---
+
+// SDA va al GPIO 21 || también puede ser 32
+// SCL va al GPIO 22 || también puede ser 33
+#define SDA_PIN 32
+#define SCL_PIN 33
+// Tamaño de pantalla
+#define SCREEN_WIDTH 128    // Ancho de la pantalla OLED, en píxeles
+#define SCREEN_HEIGHT 64   // Alto de la pantalla OLED, en píxeles
+// Dirección I2C del OLED. Los valores comunes son 0x3C o 0x3D.
+#define OLED_ADDR 0x3C
+const long SERIAL_BAUD = 9600;
+
+// Usamos -1 para el pin de RESET, ya que la mayoría de los módulos I2C no lo usan
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// --- FUNCIONES DE CHEQUEO Y DIBUJO ---
+
+bool checkI2cAddress(uint8_t address) {
+	// Intenta contactar la dirección I2C.
+	Wire.beginTransmission(address);
+	// endTransmission() devuelve 0 si el dispositivo responde con un ACK.
+	// Devuelve un valor diferente de 0 si falla (NACK).
+	return Wire.endTransmission() == 0;
+}
+
+void display_message(){
+	delay(50);
+	display.clearDisplay();
+
+	display.setTextSize(1);
+	display.setTextColor(WHITE);
+
+	display.setCursor(0, 0);
+	display.println("Test OLED ESP32 OK!");
+
+	display.setTextSize(2);
+	display.setCursor(0, 15);
+	display.println("TEST I2C");
+
+	display.setCursor(0, 50);
+	display.println("uwur");
+
+	display.display();
+}
+
+void setup() {
+	Serial.begin(SERIAL_BAUD);
+	delay(200);
+
+	// Inicializa la comunicación I2C en los pines definidos
+	Wire.begin(SDA_PIN, SCL_PIN);
+	// Reducir la velocidad del BUS a 100 kHz
+	Wire.setClock(100000);
+
+	// bool screen_found = false;
+	uint8_t final_addr = 0x00;
+
+	// 1. Verifica si la pantalla está conectada (evita el falso positivo de display.begin())
+	if (checkI2cAddress(OLED_ADDR)) {
+			final_addr = OLED_ADDR;
+	} else if (checkI2cAddress(0x3D)) {
+			final_addr = 0x3D;
+	}
+
+	// 2. Detiene el programa
+	if (final_addr == 0x00) {
+			Serial.println(F("Error: La pantalla no fue encontrada en 0x3C ni 0x3D. Posibles errores, cableado o modulo. Tus pines del ESP32 también podrían estar quemados."));
+			for(;;);
+	}
+
+	// 3. Inicialización. Solo si el hardware respondió
+	if (!display.begin(SSD1306_SWITCHCAPVCC, final_addr)) {
+			Serial.println(F("FATAL: Inicializacion de la librería falló en la direccion encontrada."));
+			for(;;);
+	} else {
+			// Informe de éxito
+			Serial.print(F("INFO: Pantalla inicializada correctamente en 0x"));
+			Serial.println(final_addr, HEX);
+
+			if (final_addr == 0x3D) {
+				 Serial.println(F("ATENCION: Se usó la direccion 0x3D. Ajusta tu codigo principal"));
+			}
+	}
+
+	display_message();
+}
+
+void loop() {
+
+}
